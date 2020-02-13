855bfb2323d31f228565e2fbf49698caeb219a37
Grzegorz Dubiel <dubielgr@gmail.com>
2020-02-13 17:34:44 +0100

    Update Toolbar.svelte

--- src/Toolbar.svelte
+++ src/Toolbar.svelte
@@ -55,7 +55,7 @@
         color: #d25577;
         outline: none;
         padding: 5px 0px 5px 5px;
-        box-shadow: 0 0 10px #d25577;
+        box-shadow: 0 0 1px #d25577;
         border: 1px solid #d25577;
     }
 
